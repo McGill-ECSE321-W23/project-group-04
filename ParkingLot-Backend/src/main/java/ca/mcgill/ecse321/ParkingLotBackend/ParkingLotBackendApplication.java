c2fea7f54f82138bc11cb979822fbbb4c61891ad
Edwin <edwin99@live.ca>
2023-02-18 16:11:14 -0500

    initial commit

---
+++ ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/ParkingLotBackend/ParkingLotBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.ParkingLotBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotBackendApplication.class, args);
+	}
+
+}
