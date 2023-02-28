4586bf430d4b9d2b264392cae054bd81813ebd9d
estefaniavazquez <estefania.vazquez@mail.mcgill.ca>
2023-02-27 22:33:41 -0500

    Commented anything using "User" class as it no longer belongs to our model

--- ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/parkinglotbackend/config/UserRepository.java
+++ ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/parkinglotbackend/config/UserRepository.java
@@ -1,10 +1,10 @@
-package ca.mcgill.ecse321.parkinglotbackend.config;
-
-import ca.mcgill.ecse321.parkinglotbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserByName(String name);
-}
+//package ca.mcgill.ecse321.parkinglotbackend.config;
+//
+//import ca.mcgill.ecse321.parkinglotbackend.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserRepository extends JpaRepository<User, Long> {
+//    User findUserByName(String name);
+//}
