7778b44f6748c35df0434f35f4951204c83170da
ElkCl0ner <33472254+ElkCl0ner@users.noreply.github.com>
2023-03-06 13:00:00 -0500

    removed unused import

--- ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/parkinglotbackend/dao/AccountRepository.java
+++ ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/parkinglotbackend/dao/AccountRepository.java
@@ -1,7 +1,6 @@
 package ca.mcgill.ecse321.parkinglotbackend.dao;
 
 import ca.mcgill.ecse321.parkinglotbackend.model.Account;
-import ca.mcgill.ecse321.parkinglotbackend.model.Person;
 
 import org.springframework.data.repository.CrudRepository;
 
