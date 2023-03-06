908e553c19a8a541d44d80659d2d6d5255a87ba8
ElkCl0ner <33472254+ElkCl0ner@users.noreply.github.com>
2023-03-05 19:24:13 -0500

    tests for person repository

--- ParkingLot-Backend/src/test/java/ca/mcgill/ecse321/parkinglotbackend/dao/PersonRepositoryTest.java
+++ ParkingLot-Backend/src/test/java/ca/mcgill/ecse321/parkinglotbackend/dao/PersonRepositoryTest.java
@@ -6,10 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import ca.mcgill.ecse321.parkinglotbackend.data.PersonMockBuilder;
 import ca.mcgill.ecse321.parkinglotbackend.model.Person;
 
+@SpringBootTest
 public class PersonRepositoryTest {
     @Autowired PersonRepository personRepository;
 
