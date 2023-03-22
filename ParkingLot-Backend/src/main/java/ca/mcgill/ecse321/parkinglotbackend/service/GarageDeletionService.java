78cea2b218564bb6b1486c6ad4e7b452c946a9fa
estefaniavazquez <estefania.vazquez@mail.mcgill.ca>
2023-03-22 03:37:10 -0400

    Add garage Service classes

---
+++ ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/parkinglotbackend/service/GarageDeletionService.java
@@ -0,0 +1,56 @@
+package ca.mcgill.ecse321.parkinglotbackend.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.mcgill.ecse321.parkinglotbackend.model.Garage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.parkinglotbackend.model.Garage;
+
+import ca.mcgill.ecse321.parkinglotbackend.dao.GarageRepository;
+
+@Service
+public class GarageDeletionService {
+    @Autowired
+    GarageRepository garageRepository;
+
+    @Transactional
+    public Garage deleteOfferedService(long garageID) throws Exception {
+        // Fetch the garage we want to delete through the id in the db
+        Garage garageService = garageRepository.findGarageByGarageID(garageID);
+
+        // If we could not find the garage to delete
+        if (garageService == null) {
+            throw new Exception("Garage not found.");
+        }
+
+        // If we found the garage to delete
+        else {
+            garageRepository.delete(garageService);
+            return garageService;
+        }
+    }
+
+    @Transactional
+    public Garage getGarageService(long garageID) {
+        // Fetch the garage we want through the id in the db
+        Garage garage = garageRepository.findGarageByGarageID(garageID);
+        return garage;
+    }
+
+    @Transactional
+    public List<Garage> getAllGarageService() {
+        return toList(garageRepository.findAll());
+    }
+
+    private <T> List<T> toList(Iterable<T> iterable) {
+        List<T> resultList = new ArrayList<T>();
+        for (T t : iterable) {
+            resultList.add(t);
+        }
+        return resultList;
+    }
+}
