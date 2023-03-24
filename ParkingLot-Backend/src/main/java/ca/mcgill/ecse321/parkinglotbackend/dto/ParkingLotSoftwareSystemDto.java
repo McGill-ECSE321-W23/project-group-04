8ee94ff7d929ccf325dbeb1df2c2675081e37521
estefaniavazquez <estefania.vazquez@mail.mcgill.ca>
2023-03-24 15:27:39 -0400

    Remove classes that hadn't been approved yet

--- ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/parkinglotbackend/dto/ParkingLotSoftwareSystemDto.java
+++
@@ -1,4 +0,0 @@
-package ca.mcgill.ecse321.parkinglotbackend.dto;
-
-public class ParkingLotSoftwareSystemDto {
-}
--- ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/parkinglotbackend/service/ParkingLotSoftwareSystemService.java
+++
@@ -1,4 +0,0 @@
-package ca.mcgill.ecse321.parkinglotbackend.service;
-
-public class ParkingLotSoftwareSystemService {
-}
