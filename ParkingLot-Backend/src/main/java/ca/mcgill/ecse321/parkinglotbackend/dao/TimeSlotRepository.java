c110ee032b3da7cdadeae7f71231abed31812f18
faizachy <faizachy18@gmail.com>
2023-03-29 17:24:30 -0400

    Revert "Merge remote-tracking branch 'origin/sprint2/System_and_TimeSlot' into Sprint2/Ticket_and_parkingSpot"
    This reverts commit f0c94c1c42db4e0fb1d752342323d4d871f82311, reversing
    changes made to 03ffd66016e16cb79efb28858c61bfd5ea2fe88e.

--- ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/parkinglotbackend/dao/TimeSlotRepository.java
+++ ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/parkinglotbackend/dao/TimeSlotRepository.java
@@ -1,14 +1,8 @@
 package ca.mcgill.ecse321.parkinglotbackend.dao;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
-
-import ca.mcgill.ecse321.parkinglotbackend.model.StaffAccount;
 import ca.mcgill.ecse321.parkinglotbackend.model.TimeSlot;
 
 public interface TimeSlotRepository extends CrudRepository<TimeSlot, Long>{
     TimeSlot findTimeSlotByTimeSlotID(Long timeSlotID);
-    List<TimeSlot> findTimeSlotByStaffAccountAccountID(Long accountID);
-    List<TimeSlot> findTimeSlotByStaffAccount(StaffAccount staffAccount);
 }
--- ParkingLot-Backend/src/main/java/ca/mcgill/ecse321/parkinglotbackend/dto/StaffAccountDto.java
+++
@@ -1,5 +0,0 @@
-package ca.mcgill.ecse321.parkinglotbackend.dto;
-
-public class StaffAccountDto {
-    
-}
