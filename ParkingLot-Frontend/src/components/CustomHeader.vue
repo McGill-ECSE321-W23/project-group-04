27f4405a027769f04aa4c1990328ebf3cd0cb055
Edwin <edwin99@live.ca>
2023-04-02 18:54:00 -0400

    fix return

---
+++ ParkingLot-Frontend/src/components/CustomHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <header style="display: flex; align-items: center;">
+        <img src="../assets/PLS_logo.png" alt="Logo" height="40px" width="auto">
+        <TabHeader text="Home" />
+        <TabHeader text="Services" />
+        <TabHeader text="My Account" />
+        <Button text="Login" color="grey" />
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+import TabHeader from './TabHeader.vue'
+
+export default {
+    name: "customHeader",
+    components: {
+        Button,
+        TabHeader
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: block;
+    width: auto;
+    padding: 20px 40px;
+    background-color: rgb(41, 41, 46);
+    font-size: 32px;
+    color: rgb(233, 231, 227);
+}
+
+#btn_Connection {
+    float: right;
+    position: relative;
+    top: 50%;
+}
+
+Button {
+    margin-left: auto;
+}
+</style>
---
+++ ParkingLot-Frontend/src/components/TabHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="tabHeader">
+        {{ text }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TabHeader",
+    props: {
+        text: String
+    }
+}
+</script>
+
+<style scoped>
+.tabHeader {
+    margin: 0px 20px;
+}
+</style>
