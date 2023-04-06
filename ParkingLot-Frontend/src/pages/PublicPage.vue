48707a4962f5f9682bf9c852d43b1542c1637ac4
anniegouchee <annie.gouchee@mail.mcgill.ca>
2023-04-06 00:23:43 -0400

    Alignment Fixes

--- ParkingLot-Frontend/src/pages/PublicPage.vue
+++ ParkingLot-Frontend/src/pages/PublicPage.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <Header :tabs="tabs"></Header>
-    <div id="page_home" class="tabBody" active="true">
-      <HomePage />
-    </div>
     <div class="pageBody">
-      <div id="page_services" class="tabBody">
+      <div id="page_home" class="tabBody" active="true">
+      <HomePage />
+      </div>
+      <div id="page_services" style= "padding: 60px 80px" class="tabBody">
         <ServiceAppointmentTab />
       </div>
-      <div id="page_reservations" class="tabBody">
+      <div id="page_reservations" style= "padding: 60px 80px" class="tabBody">
         <reservations></reservations>
       </div>
-      <div id="page_profile" class="tabBody">
+      <div id="page_profile" style= "padding: 60px 80px" class="tabBody">
         profile page
       </div>
     </div>
@@ -23,19 +23,10 @@
 
 <style scoped>
 
-#page_home{
-  display: block;
-  min-height: calc(100vh - 59px);
-  max-width: 100vw;
-  background-color: #f5f5f5;
-  color: #333;
-}
-
 div.pageBody {
   display: block;
   min-height: calc(100vh - 59px);
   max-width: 100vw;
-  padding: 60px 80px;
   background-color: #f5f5f5;
   color: #333;
 }
