01a8c33f9368952ccf8a35e714c284fa408c7672
estefaniavazquez <estefania.vazquez@mail.mcgill.ca>
2023-04-05 21:37:22 -0400

    completed serviceAppt tab for pres

--- ParkingLot-Frontend/src/pages/tabs/ServiceAppointment.js
+++ ParkingLot-Frontend/src/pages/tabs/ServiceAppointment.js
@@ -1,5 +1,7 @@
 // import axios from "axios";
 
+import { get } from "jquery";
+
 export default {
   data() {
     return {
@@ -77,14 +79,19 @@ export default {
       this.selectedRow = null;
       this.showAppointments = false;
       this.showMessage = false;
+      // this.selectedService = null;
+      // this.description = "";
+      document.getElementById("selectOfferedService").value = "";
     },
     showTable() {
-      this.showAppointments = true;
-      this.selectedService = this.offeredServicesAvailable.find((service) => {
-        return service.description === this.description;
-      });
+      if(document.getElementById("selectOfferedService").value) {
+        this.showAppointments = true;
+        this.selectedService = this.offeredServicesAvailable.find((service) => {
+          return service.description === this.description;
+        });
 
-      this.selectedService = selectedService;
+        this.selectedService = selectedService;
+      }
     }
   }
 }
