a643774527ff226269dbbcba4ae6b82f79a2c98a
ElkCl0ner <33472254+ElkCl0ner@users.noreply.github.com>
2023-04-12 21:11:12 -0400

    Frontend/add cors config (#161)
    * added extra utilities to auth controller
    
    * make header dynamic
    
    * setup cors config in backend, use axios
    
    * fix create multiple sessions
    
    * change 127... to localhost
    
    * added working login and logout
    
    * added check for isStaff
    
    * make logo click redirect to home page
    
    ---------
    
    Co-authored-by: Edwin <edwin99@live.ca>

--- ParkingLot-Frontend/src/components/Header.vue
+++ ParkingLot-Frontend/src/components/Header.vue
@@ -12,22 +12,62 @@
 
     <div class="flex-grow" />
 
-    <el-menu-item id="connection" index="connection">
+    <el-menu-item id="item_login" index="login">
       LOGIN
     </el-menu-item>
+
+    <div id="loggedInOptions" style="display: flex; flex-direction: row;">
+      <div id="greeting"></div>
+
+      <el-menu-item id="item_adminPage" index="adminPage">
+        ADMIN
+      </el-menu-item>
+
+      <el-menu-item id="item_logout" index="logout">
+        LOGOUT
+      </el-menu-item>
+    </div>
+
   </el-menu>
 </template>
 
 <script setup>
 import logo from '../assets/PLS_logo.png'
-import {ref} from 'vue'
 import $ from 'jquery'
+import router from '@/router'
 
 function handleSelect(index) {
   // Login
-  if (index === 'connection') {
-    window.location.href = '/login'
-    return
+  if (index === 'login') {
+    router.push('/login');
+    return;
+  }
+
+  // Logout
+  else if (index === 'logout') {
+    axios.post('http://localhost:8080/api/auth/logout', {}, {
+      withCredentials: true,
+      headers: {
+        "Access-Control-Allow-Origin": 'localhost:8080'
+      }
+    })
+    .then(res => {
+      console.log(res);
+      // Clear session storage
+      sessionStorage.clear();
+      // Refresh the page
+      router.go(0);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+    return;
+  }
+
+  // Admin Page
+  else if (index === 'adminPage') {
+    router.push('/admin');
+    return;
   }
 
   // Set all tab bodies to inactive
@@ -39,6 +79,8 @@ function handleSelect(index) {
 </script>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'Header',
   props: {
@@ -46,6 +88,66 @@ export default {
       type: Array,
       default: () => []
     }
+  },
+  mounted() {
+    // Logo to home page
+    $('.image').click(() => {
+      router.push('/');
+    });
+
+    // Method for displaying not logged in
+    function displayNotLoggedIn() {
+      $("#item_login").show();
+      $("#greeting").hide();
+      $("#item_adminPage").hide();
+      $("#item_logout").hide();
+    }
+
+    // Method for displaying logged in
+    function displayLoggedIn() {
+      $("#item_login").hide();
+      $("#greeting").show();
+      $("#item_logout").show();
+      axios.get('http://localhost:8080/api/auth/isStaff', {
+        withCredentials: true,
+        headers: {
+          "Access-Control-Allow-Origin": 'localhost:8080'
+        }
+      })
+      .then(res => {
+        console.log(res);
+        if (res.data === true) {
+          $("#item_adminPage").show();
+        } else {
+          $("#item_adminPage").hide();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+
+    // Check if the user is logged in
+    axios.get('http://localhost:8080/api/auth/isLoggedIn', {
+      withCredentials: true,
+      headers: {
+        "Access-Control-Allow-Origin": 'localhost:8080'
+      }
+    })
+    .then(res => {
+      console.log(document.cookie)
+      console.log(res);
+      var loggedIn = res.data === true;
+      console.log(loggedIn);
+      if (loggedIn) {
+        displayLoggedIn();
+      } else {
+        displayNotLoggedIn();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 }
 </script>
@@ -54,6 +156,7 @@ export default {
 .image {
   height: 58px;
   margin: 0 20px;
+  cursor: pointer;
 }
 
 .flex-grow {
